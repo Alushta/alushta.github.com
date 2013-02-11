6cc82da6668afa445be0b7ca432c9395b431b180
Sergey Gerasimov <mail@grsmv.com>
2013-02-12 01:08:59 +0200

    Statistics, coding style for JS

--- common.js
+++ common.js
@@ -1,74 +1,72 @@
-/**
- *  Helper for selectors
- */
-var $ = function (selector) {
-  return document.getElementById(selector);
-};
+(function(){
+  
+  window.addEventListener('load', window_load_handler, false);
 
-var BigPicture = {
-  open_holder: function () {
-    $("big_picture_holder").style.display = "table";
-    $("big_picture_help").style.display = "block";
-  },
 
-  close_holder: function () {
-    $("big_picture_holder").style.display = "none";
-    $("big_picture_help").style.display = "none";
-  },
-
-  place_image: function (image_name) {
-    var image = $("big_picture").getElementsByTagName("img")[0];
-    image.src = "images/big/" + image_name;
-    image.onclick = function () {
-      BigPicture.close_holder();
-    };
-
-    image.onload = function() {
-      if (this.height > window.innerHeight) {
-        this.height = window.innerHeight - 30;
-      }
-    };
+  // access point
+  function window_load_handler () {
+    activate_thumbs ();
   }
-};
 
-/**
- *  Onlick actions for each thumb
- */
-var open_big_picture = function() {
-  var bp = BigPicture;
-  var image_name = this.getElementsByTagName("img").length > 0 ? 
-    this.getElementsByTagName("img")[0].src.split("/").pop() : "";
 
-  bp.open_holder();
-  bp.place_image(image_name);
-};
+  // helper for selectors 
+  function $ (selector) {
+    return document.getElementById(selector);
+  }
 
-/**
- *  Setting actions for each thumb
- */
-var set_click_handler_for_thumbs = function() {
-  var i, j, gallery_divs = $("gallery").getElementsByTagName("div");
 
-  for (i in gallery_divs) {
-    if (gallery_divs.hasOwnProperty(i)) {
-      if (gallery_divs[i].className === 'thumbs') {
+  // setting actions for each thumb
+  function activate_thumbs () {
+    var i, j, gallery_divs = $("gallery").getElementsByTagName("div");
 
-        var thumbs = gallery_divs[i].getElementsByTagName("div");
-            
-        for (j in thumbs) {
-          if (thumbs.hasOwnProperty(j)) {
-            if (typeof thumbs[j] === 'object') {
-              thumbs[j].onclick = open_big_picture;
-            }
-          }
+    for (i in gallery_divs) 
+      if (gallery_divs.hasOwnProperty(i)) 
+        if (gallery_divs[i].className === 'thumbs') {
+
+          var thumbs = gallery_divs[i].getElementsByTagName("div");
+          for (j in thumbs) 
+            if (thumbs.hasOwnProperty(j)) 
+              if (typeof thumbs[j] === 'object') 
+                thumbs[j].addEventListener('click', big_picture.open, false);
         }
-      }
-    }
   }
-};
 
-var main = function() {
-  set_click_handler_for_thumbs();
-};
 
-window.onload = main;
+  // toolset for viewing zoomed pictures
+  var big_picture = {
+    
+    open: function () {
+      var bp = big_picture;
+      var image_name = this.getElementsByTagName("img").length > 0 ? 
+        this.getElementsByTagName("img")[0].src.split("/").pop() : "";
+
+      big_picture.open_holder();
+      big_picture.place_image(image_name);
+    },
+
+    open_holder: function () {
+      $("big_picture_holder").style.display = "table";
+      $("big_picture_help").style.display = "block";
+    },
+
+    close_holder: function () {
+      $("big_picture_holder").style.display = "none";
+      $("big_picture_help").style.display = "none";
+    },
+
+    place_image: function (image_name) {
+      var image = $("big_picture").getElementsByTagName("img")[0];
+      image.src = "images/big/" + image_name;
+      image.addEventListener('click', function() {
+        big_picture.close_holder();
+      }, false);
+
+      image.onload = function() {
+        if (this.height > window.innerHeight) {
+          this.height = window.innerHeight - 30;
+        }
+      };
+    }
+  };
+
+}());
